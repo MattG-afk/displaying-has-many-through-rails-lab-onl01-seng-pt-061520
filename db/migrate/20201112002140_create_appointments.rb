766e7ecd3ed22fc99958e058e3036645b22ac29e
Matt Gordon <bigmattsize21@gmail.com>
2020-11-12 00:22:18 +0000

    Automatically backed up by Learn

--- db/migrate/20201112002140_create_appointments.rb
+++ db/migrate/20201112002140_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.belongs_to 
+      t.belongs_to :doctor, foreign_key: true
       t.belongs_to
 
       t.timestamps
